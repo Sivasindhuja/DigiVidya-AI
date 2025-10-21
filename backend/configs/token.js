bbe4768936c86de76ee02bda07bda5c073c53a83
Sivasindhuja <160160724+Sivasindhuja@users.noreply.github.com>
2025-10-21 17:10:13 +0530

    Fix variable declaration for token in genToken function

--- backend/configs/token.js
+++ backend/configs/token.js
@@ -1,7 +1,7 @@
 import jwt from "jsonwebtoken"
 export const genToken= async(userId)=>{
 try {
-   let token= jwt.sign({userId} , process.env.JWT_SECRET , {expiresIn:"7d"})
+   const token= jwt.sign({userId} , process.env.JWT_SECRET , {expiresIn:"7d"})
    return token
 } catch (error) {
     console.log("token error")
